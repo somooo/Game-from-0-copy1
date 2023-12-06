9d9dbf27a42ee348ec31ef6f87ed5b575ec0e976
Norvh <norah.wadani@gmail.com>
2023-12-06 15:48:38 +0300

    00

---
+++ Game from 0/Preview Content/result.swift
@@ -0,0 +1,18 @@
+//
+//  result.swift
+//  Game from 0
+//
+//  Created by Norah W on 22/05/1445 AH.
+//
+
+import SwiftUI
+
+struct result: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    result()
+}
